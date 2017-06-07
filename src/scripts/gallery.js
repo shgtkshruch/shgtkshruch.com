abca9b277cfb543a94d40d97fda333fb123c3468
shgtkshruch <shgtk.shruch@gmail.com>
2017-06-07 17:28:28 +0900

    Add read more button

--- src/scripts/gallery.js
+++ src/scripts/gallery.js
@@ -11,8 +11,11 @@ export default () => {
 
     $('#typed').addClass('is-text');
 
+    $images.removeClass('is-active');
+    $(this).addClass('is-active');
+
     Typed.new('#typed', {
-      strings: [`title: ${title}<br>age: ${age}<br>url: <a href="${url}" target="_blank">${url}</a><br>${desc}`],
+      strings: [`title: ${title}<br>age: ${age}<br>url: <a href="${url}" target="_blank">${url}</a><br>${desc}<br><a href="#" class="btn">more</a>`],
       showCursor: false,
     });
   });
