b5493e0eb5d873a9816583e923ccb42b1da83a64
shgtkshruch <shgtk.shruch@gmail.com>
2017-06-07 18:57:07 +0900

    Change how to pass data in gallery item

--- src/scripts/gallery.js
+++ src/scripts/gallery.js
@@ -1,21 +1,12 @@
 /* global Typed */
 
-function h(text) {
-  return `<p class="text"><span class="text__content">${text}</p><br>`;
-}
-
 export default () => {
   const $images = $('.js-gallery-item');
 
   $images.click(function i() {
-    const title = $(this).data('title');
-    const age = $(this).data('age');
-    const url = $(this).data('url');
-    const desc = $(this).data('desc');
-
     $('#typed')
       .addClass('is-text')
-      .html(`${h(title) + h(age) + h(url) + h(desc)}<a href="#" class="btn">more</a>`);
+      .html($(this).find('.js-gallery-data').html());
 
     $images.removeClass('is-active');
     $(this).addClass('is-active');
