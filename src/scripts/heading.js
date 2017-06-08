0642290e3e04a95eec83f3c69183690e7022b79f
shgtkshruch <shgtk.shruch@gmail.com>
2017-06-08 18:55:28 +0900

    Start animation if previous section animation end

--- src/scripts/heading.js
+++ src/scripts/heading.js
@@ -14,16 +14,33 @@ export default () => {
     bottom: $(window).height() / 3,
   });
 
+  let introShow = false;
+  let workIsShow = false;
+  let historyIsShow = false;
+  let skillIsShow = false;
+
   inView('.js-heading')
     .on('enter', (el) => {
       if (!$(el).find('.js-heading-output').is(':empty')) return;
+
+      if (
+        ($(el).hasClass('work') && !introShow) ||
+        ($(el).hasClass('history') && !workIsShow) ||
+        ($(el).hasClass('skill') && !historyIsShow) ||
+        ($(el).hasClass('contact') && !skillIsShow)
+      ) return;
 
       Typed.new(`#${el.id} .js-heading-output`, {
         stringsElement: $(el).find('.heading__main').get(0),
         showCursor: false,
         callback() {
+          if ($(el).hasClass('intro')) {
+            introShow = true;
+          }
+
           if ($(el).hasClass('work')) {
             $('.work__list').fadeIn();
+            workIsShow = true;
           }
 
           if ($(el).hasClass('history')) {
@@ -31,12 +48,14 @@ export default () => {
               $('.history__list').addClass('is-active');
               setTimeout(() => {
                 $('.history__item').first().find('.history__name').click();
+                historyIsShow = true;
               }, 1800);
             }, 500);
           }
 
           if ($(el).hasClass('skill')) {
             $('.skill__list').addClass('is-active');
+            skillIsShow = true;
           }
 
           if ($(el).hasClass('contact')) {
