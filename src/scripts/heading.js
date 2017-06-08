a75041ef600efc3980a68760caf9f366fe3c60cb
shgtkshruch <shgtk.shruch@gmail.com>
2017-06-08 10:29:43 +0900

    Add intro section

--- src/scripts/heading.js
+++ src/scripts/heading.js
@@ -3,7 +3,7 @@
 export default () => {
   inView('.js-heading')
     .on('enter', (el) => {
-      Typed.new('.heading__typed', {
+      Typed.new(`#${el.id} .heading__typed`, {
         stringsElement: $(el).find('.heading__main').get(0),
         showCursor: false,
       });
