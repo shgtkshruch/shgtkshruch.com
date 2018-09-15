e5ce416d29ef8379a88a10915be937a35cc5ce5c
shgtkshruch <shgtk.shruch@gmail.com>
2018-09-15 22:54:18 +0900

    Lazy load work images
    close #2

--- src/scripts/work.js
+++ src/scripts/work.js
@@ -7,5 +7,11 @@ export default () => {
         .addClass('is-active')
         .siblings('.js-work-data').show();
     });
+
+  window.addEventListener('load', () => {
+    $('.work').find('.work__img').each((i, el) => {
+      $(el).attr('src', $(el).data('src'));
+    });
+  });
 };
 
