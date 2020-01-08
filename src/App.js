210a198f61496c92626cd51d3e45d6b1a6f091eb
shgtkshruch <shgtk.shruch@gmail.com>
2020-01-08 22:00:25 +0900

    Add container

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
 
+import Container from './components/Container'
 import Work from './Work'
 
 export default () => {
   return (
-    <>
+    <Container>
       <link href="https://fonts.googleapis.com/css?family=Sawarabi+Gothic" rel="stylesheet" />
       <link href="https://fonts.googleapis.com/css?family=Inconsolata" rel="stylesheet" />
       <Work />
-    </>
+    </Container>
   );
 }
---
+++ src/components/Container.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import styled from '@emotion/styled'
+
+import { mq } from '../variables'
+
+const Container = styled.div`
+  max-width: 48rem;
+  margin-left: auto;
+  margin-right: auto;
+  padding-right: 1rem;
+  padding-left: 1rem;
+
+  ${mq.pc} {
+    max-width: 100%;
+    padding-right: 4rem;
+    padding-left: 4rem;
+  }
+`
+
+export default ({ children }) => {
+  return (
+    <Container>{ children }</Container>
+  )
+}
