e859cdb4ff528d71b71c619f8369bda0f243b2fb
shgtkshruch <shgtk.shruch@gmail.com>
2020-01-10 15:10:14 +0900

    Show mouse to intro section after typing animation end

--- src/Intro.js
+++ src/Intro.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from '@emotion/styled'
 import Hgroup from './components/Hgroup'
 import Mouse from './Mouse'
@@ -19,18 +19,30 @@ const MouseWrapper = styled.div`
   bottom: 3rem;
   left: 50%;
   transform: translate(-50%, 0);
+  opacity: ${props => props.isMouseShow ? 1 : 0};
+  transition: opacity 0.7s ease-in;
 `
 
-export default () => (
-  <Section>
-    <Wrapper>
-      <Hgroup
-        title="Shigetaka Shirouchi"
-        subTitle="I'm a Front-End Engineer."
-      />
-    </Wrapper>
-    <MouseWrapper>
-      <Mouse />
-    </MouseWrapper>
-  </Section>
-)
+export default () => {
+  const [isMouseShow, setIsMouseShow] = useState(false)
+
+  function onTypingDone() {
+    setIsMouseShow(true)
+    window.addEventListener('scroll', () => setIsMouseShow(false))
+  }
+
+  return (
+    <Section>
+      <Wrapper>
+        <Hgroup
+          title="Shigetaka Shirouchi"
+          subTitle="I'm a Front-End Engineer."
+          onTypingDone={onTypingDone}
+        />
+      </Wrapper>
+      <MouseWrapper isMouseShow={isMouseShow}>
+        <Mouse />
+      </MouseWrapper>
+    </Section>
+  )
+}
