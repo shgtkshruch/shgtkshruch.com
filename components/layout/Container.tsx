a54926ae967d3729240b78be70a540140cb49e7f
shgtkshruch <shgtk.shruch@gmail.com>
2020-06-06 06:48:13 +0900

    Add Container.tsx

---
+++ components/layout/Container.tsx
@@ -0,0 +1,19 @@
+import React from "react"
+import styled from "@emotion/styled"
+
+import { mq } from "../variables"
+
+const Container = styled.div`
+  max-width: 48rem;
+  margin-left: auto;
+  margin-right: auto;
+  padding-right: 1.5rem;
+  padding-left: 1.5rem;
+  ${mq.pc} {
+    max-width: 98rem;
+    padding-right: 4rem;
+    padding-left: 4rem;
+  }
+`;
+
+export default ({ children }) => <Container>{children}</Container>;
---
+++ components/variables.ts
@@ -0,0 +1,26 @@
+const breakpoints = { sp: 600,  pc:1200 }
+
+type MQ = {
+  sp: string
+  pc: string
+}
+
+const mq: MQ = Object.keys(breakpoints).reduce((obj, type) => {
+  obj[type] = `@media (min-width: ${breakpoints[type]}px)`
+  return obj
+}, {})
+
+const theme = {
+  light: {
+    primaryColor: '#222',
+    bgColor: '#fff',
+    accentColor: 'red',
+  },
+  dark: {
+    primaryColor: '#dee1e8',
+    bgColor: '#161725',
+    accentColor: '#ef40b8',
+  }
+}
+
+export { breakpoints, mq, theme };
