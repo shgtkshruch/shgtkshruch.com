69dade831313a5cf7f233d31eda0e21f6ef1e812
shgtkshruch <shgtk.shruch@gmail.com>
2020-12-05 16:17:32 +0900

    Add Lighthouse CI
    - https://github.com/GoogleChrome/lighthouse-ci#quick-start
    - [Collect Lighthouse Results](https://github.com/GoogleChrome/lighthouse-ci/blob/master/docs/getting-started.md#collect-lighthouse-results)

---
+++ lighthouserc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  ci: {
+    collect: {
+      staticDistDir: './out',
+    },
+    // TODO: pass recommended assertion
+    // assert: {
+    //   preset: "lighthouse:recommended",
+    // },
+    upload: {
+      target: 'temporary-public-storage',
+    },
+  },
+};
